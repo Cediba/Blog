0208ca85ef52d991fda414552a796bf91ddb0849
Cedric <cedric.balmer@gmx.ch>
2020-02-12 14:56:27 -0800

    added style

---
+++ controllers/controller.js
@@ -0,0 +1,61 @@
+const Post = require('../models/post');
+
+exports.getHomePage = (req, res, next) => {
+    Post.getAll().then(posts => {
+        res.render('home', { posts });
+    });
+}
+
+exports.getFormPage = (req, res, next) => {
+    const { id } = req.params;
+    const isEditing = !!id;
+    let post = null;
+
+    if (isEditing) {
+        Post.getById(id).then(post => {
+            res.render('form', { post });
+        });
+    } else {
+        res.render('form', { post });
+    }
+}
+
+exports.addPost = (req, res, next) => {
+    const { author, title, content } = req.body;
+    const post = new Post(author, title, content);
+    post.addPost();
+    res.redirect('/');
+}
+
+exports.updatePost = (req, res, next) => {
+    const { id, title, content } = req.body;
+
+    Post.updatePost(id, title, content);
+    res.redirect('/posts/' + id);
+}
+
+exports.getPostDetailPage = (req, res, next) => {
+    const { id } = req.params;
+
+    Post.getById(id).then(post => {
+        res.render('post-detail', { post });
+    });
+}
+
+exports.deletePost = (req, res, next) => {
+    const { id } = req.params;
+    Post.deletePost(id);
+    res.redirect('/');
+}
+
+exports.deleteAllPosts = (req, res, next) => {
+    Post.deleteAllPosts();
+    res.redirect('/');
+};
+
+exports.saveComment = (req, res, next) => {
+    const { id } = req.params
+    const { name, content } = req.body;
+    Post.saveComment(id, name, content);
+    res.redirect('/posts/' + id);
+}
